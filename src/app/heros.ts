d971ed66c5c7df51a0607b6fba3ea0504167b319
yeli <631300329@qq.com>
2016-11-18 16:19:56 +0800

    hero

---
+++ src/app/heros.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by admin on 2016/11/16.
+ */
+export class Heros {
+    id:number;
+    name:string;
+    power:string;
+    alterEgo?:string;
+
+    constructor(
+        private id_in: number,
+        private name_in: string,
+        private power_in: string,
+        private alterEgo_in?: string
+    ) {
+        this.id=id_in;
+        this.name=name_in;
+        this.power=power_in;
+        this.alterEgo=alterEgo_in;
+    }
+}
