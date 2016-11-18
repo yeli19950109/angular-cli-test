e66480f9328506e0cff574bea5f4269f4b381762
yeli <631300329@qq.com>
2016-11-18 12:07:33 +0800

    添加

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by admin on 2016/11/18.
+ */
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {HeroFormComponent}      from './hero-form.component';
+import { CrisisListComponent }  from './crisis-list.component';
+
+const routes: Routes = [
+    { path: 'hero/form', component: HeroFormComponent},
+    { path: 'crisis-center', component: CrisisListComponent },
+    { path: '', redirectTo: '/hero/form', pathMatch: 'full' }
+];
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
+export const routedComponents = [HeroFormComponent];
---
+++ src/app/crisis-list.component.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by admin on 2016/11/18.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    //moduleId: module.id,
+    selector: 'crisis-list',
+    templateUrl: 'crisis-list.component.html'
+    // template: `
+    // <h2>CRISIS CENTER</h2>
+    // <p>Get your crisis here</p>`
+})
+export class CrisisListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
+// export class CrisisListComponent { }
