de7936b760815880a5227133048b9e38ca8d88a9
yeser <631300329@qq.com>
2016-11-23 10:31:48 +0800

    添加子模块

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import {Component} from '@angular/core';
         <h2 highlight>angular-cli-test</h2>
         <nav>
             <a routerLink="/hero/form" routerLinkActive="active">hreo-form</a>
+            <a routerLink="/contact" routerLinkActive="active">Contact</a>
             <a routerLink="/crisis-center" routerLinkActive="active">Crisis Center</a>
             <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
             <a routerLink="/admin" routerLinkActive="active">Admin</a>
             <a routerLink="/login" routerLinkActive="active">Login</a>
         </nav>
+        
         <router-outlet></router-outlet>
         <!--<hero-form></hero-form>-->
     </div>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import {BrowserModule}      from '@angular/platform-browser';
-import {NgModule}           from '@angular/core';
-import {FormsModule}        from '@angular/forms';
-import {HttpModule}         from '@angular/http';
+import {BrowserModule}          from '@angular/platform-browser';
+import {NgModule}               from '@angular/core';
+import {FormsModule}            from '@angular/forms';
+import {HttpModule}             from '@angular/http';
 
-import {AppRoutingModule}   from './app-routing.module';
-import {AppComponent}       from './app.component';
-import {HeroFormComponent}  from './hero-form.component';
-import { HeroesModule }     from './heros/heroes.module';
-//import { CrisisListComponent } from './crisis/crisis-list.component';
-import { CrisisModule }     from './crisis/crisis.module';
-import { AdminModule }      from './admin/admin.module';
+import {AppRoutingModule}       from './app-routing.module';
+import { HeroesModule }         from './heros/heroes.module';
+import { CrisisModule }         from './crisis/crisis.module';
 import { LoginRoutingModule }   from './login-routing.module';
+import { ContactModule }        from './contact/contact.module';
+import {AppComponent}           from './app.component';
+import {HeroFormComponent}      from './hero-form.component';
 import { LoginComponent }       from './login.component';
-import { HighlightDirective} from './highlight.directive';
+import { HighlightDirective}    from './highlight.directive';
 
-import {DialogService}from './dialog.service';
+import {DialogService}          from './dialog.service';
+import {UserService}            from './user.service';
 
 @NgModule({
     declarations: [
@@ -31,10 +31,12 @@ import {DialogService}from './dialog.service';
         AppRoutingModule,
         HeroesModule,
         CrisisModule,
-        //AdminModule
+        ContactModule
+        //AdminModule 利用路由懒加载
     ],
     providers: [
-        DialogService
+        DialogService,
+        UserService
     ],
     bootstrap: [
         AppComponent
