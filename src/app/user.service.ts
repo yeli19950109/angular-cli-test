a8b17b89166b28cf191602ee706894587912c37f
yeser <631300329@qq.com>
2016-11-23 10:31:14 +0800

    用户相关的操作

---
+++ src/app/user.service.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class UserService {
+    public userName="admin";
+    public id=1;
+
+    constructor() {
+    }
+}
